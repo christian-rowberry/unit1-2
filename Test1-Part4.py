f378225ca5ecde2ca6b49c73bcae9f799c40f3a5
christian-rowberry <44683708+christian-rowberry@users.noreply.github.com>
2018-11-02 13:44:30 -0700

    Delete Test1-Part4.py

--- Test1-Part4.py
+++
@@ -1,6 +0,0 @@
-def centuryFromYear(year):
-    return (year) // 100 + 1
-
-
-userYear = input("What Year would you like to convert ?>> ")
-print("Year: " + str(userYear) + " is to the " + str(centuryFromYear(int(userYear))) + "-th Century")
